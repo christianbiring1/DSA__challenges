c4bccc13fd90c44b959cfe53fc1f886d5e864114
christian <christianbiringanine22@gmail.com>
2023-05-18 13:02:07 +0200

    Refactor  getMax challenge using the reduce method

--- getMax.js
+++ getMax.js
@@ -1,6 +1,7 @@
 // Get the maximum number in an array of numbers
 
 const getMax = (arr) => {
+  if (arr.length === 0) return undefined;
   // let max = 0;
   // for (let i = 0; i < arr.length; i += 1) {
   //   if (arr[i] > max) max = arr[i];
@@ -13,4 +14,4 @@ const getMax = (arr) => {
   });
 };
 
-console.log(getMax([1, 1, 2, 4, 5]));
+console.log(getMax([]));
