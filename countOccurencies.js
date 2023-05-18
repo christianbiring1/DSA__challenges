9f26badff7ca79a10f95f4833e91b5c7a158711c
christian <christianbiringanine22@gmail.com>
2023-05-18 12:48:26 +0200

    Solve getMax challenge

--- countOccurencies.js
+++ countOccurencies.js
@@ -1,12 +1,9 @@
-// const coutOccurencies = (array, searchElement) => {
-//   let count = 0;
-//   for (let i of array) if (i === searchElement) count += 1;
-//   return count;
-// };
-
-// OR
-
 const coutOccurencies = (array, searchElement) => {
+  // let count = 0;
+  // for (let i of array) if (i === searchElement) count += 1;
+  // return count;
+
+  // OR
   return array.reduce((accumulator, currentValue) => {
     const occurency = currentValue === searchElement ? 1 : 0;
     return occurency + accumulator;
---
+++ getMax.js
@@ -0,0 +1,11 @@
+// Get the maximum number in an array of numbers
+
+const getMax = (arr) => {
+  let max = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] > max) max = arr[i];
+  }
+  return max;
+};
+
+console.log(getMax([1, 1, 2, 4, 5]));
