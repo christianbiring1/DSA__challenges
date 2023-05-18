d03c023ca77bac37dd1e40f9b114a5848f173bdb
christian <christianbiringanine22@gmail.com>
2023-05-18 12:38:27 +0200

    Solve countOccurancies challenge with the reduce method

---
+++ countOccurencies.js
@@ -0,0 +1,16 @@
+// const coutOccurencies = (array, searchElement) => {
+//   let count = 0;
+//   for (let i of array) if (i === searchElement) count += 1;
+//   return count;
+// };
+
+// OR
+
+const coutOccurencies = (array, searchElement) => {
+  return array.reduce((accumulator, currentValue) => {
+    const occurency = currentValue === searchElement ? 1 : 0;
+    return occurency + accumulator;
+  });
+};
+
+console.log(coutOccurencies([1, 2, 3, 1, 1, 1], 1));
