f0be0ae979ea3bb012f973f451e8abcad427d78a
christian <christianbiringanine22@gmail.com>
2023-05-31 11:26:53 +0200

    Solve the shownumbers ODD or EVEN

--- demerite.js
+++ demerite.js
@@ -11,3 +11,12 @@ function checkSpeed(speed) {
 }
 
 checkSpeed(115);
+
+function showNumbers(limit) {
+  for (let i = 0; i <= limit; i += 1) {
+    const message = i % 2 === 0 ? "EVEN" : "ODD";
+    console.log(i, message);
+  }
+}
+
+showNumbers(10);
