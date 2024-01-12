52b9e10edc821b9052ce98be945f1afda7159955
christian <christianbiringanine22@gmail.com>
2024-01-12 11:57:06 +0200

    Add more cs

--- codeSignal/square.js
+++ codeSignal/square.js
@@ -14,4 +14,47 @@ function drawSquare(n) {
   return output
 }
 
-console.log(drawSquare(7))
+// console.log(drawSquare(7))
+
+function MyFunction(arr1, arr2) {
+  const n = arr1.length;
+  const result = [];
+
+  for(let i = 0; i < n; i += 1) {
+    let sum = 0;
+    const shiftArray = arr1.slice(i).concat(arr1.slice(0, i));
+    console.log(shiftArray)
+
+    for(let j = 0; j < n; j += 1) {
+      sum += Math.abs(shiftArray[j] + arr2[j])
+    }
+    result.push(sum);
+  }
+
+  return result
+}
+
+// console.log(MyFunction([1,-2,3], [1,2,-3]))
+
+
+function MyString(str1, str2) {
+  for(let i = 0; i < str1.length; i += 1) {
+    let char1 = str1.charCodeAt(i);
+    let char2 = str2.charCodeAt(i+1)
+    console.log(char1, char2)
+
+    if(char1 === 122 && char2 === 97) {
+      char2 = 123
+    }
+    if(char1 > char2) {
+      return false
+    }
+  }
+  return true
+}
+
+console.log(MyString('abc', 'ccd'))
+// console.log(MyString('xyz', 'yza'))
+// console.log(MyString('efg', 'fgh'))
+
+
